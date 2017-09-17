42f38fa1823f6c14d01939f6bd5aa367e4c6c13f
Yu Feng <rainwoodman@gmail.com>
2017-09-16 23:25:18 -0700

    oops forgot the templates.

---
+++ recipe-templates/ngspice/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ $OSTYPE == darwin* ]]; then
+     export CFLAGS="-headerpad_max_install_names"
+     export CXXFLAGS=$CFLAGS
+fi
+
+./configure --prefix=$PREFIX --with-x --with-readline
+
+make -j$CPU_COUNT
+make install
