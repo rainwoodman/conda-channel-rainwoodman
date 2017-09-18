0f23dc7fbd6aee36e4399238b6df106db6ef8aff
Yu Feng <rainwoodman@gmail.com>
2017-09-17 17:17:19 -0700

    set LDFLAGS on mac.

--- recipe-templates/ngspice/build.sh
+++ recipe-templates/ngspice/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ $OSTYPE == darwin* ]]; then
-     export CFLAGS="-headerpad_max_install_names"
-     export CXXFLAGS=$CFLAGS
+    export CFLAGS="-headerpad_max_install_names"
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    export CXXFLAGS=$CFLAGS
 fi
 
 ./configure --prefix=$PREFIX --with-x --with-readline
